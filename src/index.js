f2b6dc63b270aec659ce6490ae8f8050516c1222
Kirill Kravinsky <woyorus@gmail.com>
2016-07-23 11:03:07 -0400

    code refactoring

--- src/index.js
+++ src/index.js
@@ -12,13 +12,17 @@ function mixin(obj) {
     for (var key in ObjectFsm.prototype) {
         obj[key] = ObjectFsm.prototype[key];
     }
+    initialize(obj);
+    return obj;
+}
+
+function initialize(obj) {
     obj.fsm = {};
     obj.fsm.states = [];
     obj.fsm.events = {};
     obj.fsm.transitionCancelled = false;
     obj.fsm.transitionDeferred = false;
     obj.fsm.switchingStates = false;
-    return obj;
 }
 
 ObjectFsm.prototype.addState = function (states) {
@@ -31,13 +35,17 @@ ObjectFsm.prototype.addState = function (states) {
 };
 
 ObjectFsm.prototype.setStartingState = function (state) {
-    if (this.stateExists(state)) {
+    if (this.hasState(state)) {
         this.state = state;
     }
 };
 
-ObjectFsm.prototype.stateExists = function (state) {
+ObjectFsm.prototype.hasState = function (state) {
     return this.fsm.states.indexOf(state) !== -1;
+};
+
+ObjectFsm.prototype.hasEvent = function (event) {
+    return this.fsm.events.hasOwnProperty(event);
 };
 
 ObjectFsm.prototype.addEvent = function(eventName, statesFrom, stateTo, handler) {
@@ -55,33 +63,35 @@ ObjectFsm.prototype.addEvent = function(eventName, statesFrom, stateTo, handler)
 };
 
 ObjectFsm.prototype.handleEvent = function (event) {
-    if (!this.fsm.switchingStates) {
-        if (this.fsm.events.hasOwnProperty(event)) {
-            var eventObject = this.fsm.events[event];
-            if (eventObject.validFrom.indexOf(this.state) !== -1) {
-                this.fsm.switchingStates = true;
-                var fromState = this.state;
-                var toState = eventObject.validTo;
-                var handlerReturnValue = undefined;
-                this.emit('willTransition', fromState, toState, event);
-                if (typeof eventObject.handler === 'function') {
-                    var handlerArgs = Array.prototype.slice.call(arguments).slice(1);
-                    handlerReturnValue = eventObject.handler.apply(this, handlerArgs);
-                }
-                var that = this;
-                this.fsm.finalizeTransitionClosure = function () {
-                    if (!that.fsm.transitionCancelled) {
-                        that.state = toState;
-                        that.emit('didTransition', fromState, toState, event);
-                    }
-                };
-                if (!this.fsm.transitionDeferred) {
-                    this.finalizeTransition();
-                }
-                return handlerReturnValue;
-            }
+    if (this.canHandleEvent(event)) {
+        var eventObject = this.fsm.events[event];
+        var fromState = this.state;
+        var toState = eventObject.validTo;
+        var handlerReturnValue = undefined;
+        this.fsm.switchingStates = true;
+        this.emit('willTransition', fromState, toState, event);
+        if (typeof eventObject.handler === 'function') {
+            var handlerArgs = Array.prototype.slice.call(arguments).slice(1);
+            handlerReturnValue = eventObject.handler.apply(this, handlerArgs);
         }
+        var that = this;
+        this.fsm.finalizeTransitionClosure = function () {
+            if (!that.fsm.transitionCancelled) {
+                that.state = toState;
+                that.emit('didTransition', fromState, toState, event);
+            }
+        };
+        if (!this.fsm.transitionDeferred) {
+            this.finalizeTransition();
+        }
+        return handlerReturnValue;
     }
+};
+
+ObjectFsm.prototype.canHandleEvent = function (event) {
+    return this.hasEvent(event) &&
+        (this.fsm.events[event].validFrom.indexOf(this.state) !== -1) &&
+            this.fsm.switchingStates === false;
 };
 
 ObjectFsm.prototype.cancelTransition = function () {
